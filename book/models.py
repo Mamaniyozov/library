44d7bed9b1136c9385055b3b8d13c2d32b3102fa
Mamaniyozov <mamaniyozovmuhammadyusuf5@gmail.com>
2023-10-21 09:16:48 +0500

    add in models Publisher

--- book/models.py
+++ book/models.py
@@ -7,5 +7,9 @@ class Genre(models.Model):
     def __str__(self):
         return self.name
 
-
+class Publisher (models.Model):
+    name= models.CharField(max_length=200, unique=True)
+    
+    def __str__(self):
+        return self.name
 # Create your models here.
