fdcea6364a88c839822a4dff116a001f4a485ac1
Mamaniyozov <mamaniyozovmuhammadyusuf5@gmail.com>
2023-10-26 12:41:38 +0500

    get all users

--- book/views.py
+++ book/views.py
@@ -4,4 +4,11 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.request import Request
 from rest_framework.views import APIView
+class Users(APIView):
+    def get(self, requsets):
+        try:
+            user = User.objects.all()
+            return Response({"username": user.username, "first_name": user.first_name, "last_name": user.last_name})
+        except:
+            return Response({'result': ' Users not found'})
 # Create your views here.
