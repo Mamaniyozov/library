268a3aa63f09875e9ff05218c5fa5458e65f7c5c
Mamaniyozov <mamaniyozovmuhammadyusuf5@gmail.com>
2023-10-26 16:19:20 +0500

    init

--- book/views.py
+++ book/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import User
-from .models import Book,Author,Genre,Users,User_id,Publisher,Language, creat_user, login_user, Reaction
+from .serializers import Book,Author,Genre,Users,User_id,Publisher,Language, creat_user, login_user, Reaction
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.request import Request
