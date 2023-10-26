06ad2cd2153c4ee3a7e6950c49aab29f646c6223
Mamaniyozov <mamaniyozovmuhammadyusuf5@gmail.com>
2023-10-26 16:18:07 +0500

    all in bag fixed

--- book/admin.py
+++ book/admin.py
@@ -1,24 +1,16 @@
 from django.contrib import admin
 
-from .models import Genre, Publisher, Author, Book, User ,User_id , login_user ,Language  ,Reaction
+from .models import Genre, Publisher, Author, Book, Users ,User_id , login_user ,Language  ,Reaction
 
 # admin.site.register(User)
 
-admin.site.register(Genre)
+admin.site.register([Genre,
+                    Publisher, 
+                    Author,
+                    Book, 
+                    Users ,
+                    User_id , 
+                    login_user ,
+                    Language  ,
+                    Reaction])
 
-admin.site.register(User)
-
-admin.site.register(Publisher)
-
-admin.site.register(Author)
-
-admin.site.register(User_id)
-
-admin.site.register(Book)
-
-admin.site.register(Language)
-
-admin.site.register(login_user)
-
-admin.site.register(Reaction)
-# Register your models here.
