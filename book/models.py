75969faee6d44c3a7d0a8bb129eab4ba77ec8773
Mamaniyozov <mamaniyozovmuhammadyusuf5@gmail.com>
2023-10-26 12:40:39 +0500

    create lastname in user

--- book/models.py
+++ book/models.py
@@ -65,7 +65,7 @@ class Book (models.Model):
 class User(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     def __str__(self):
-        return self.user.username
+        return self.user.username, self.user.lastname
 
 
 class User_id(models.Model):
--- book/views.py
+++ book/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
-
+from django.contrib.auth.models import User
+from .models import Book,Genre,Language,Author,Publisher, Reaction
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.request import Request
+from rest_framework.views import APIView
 # Create your views here.
