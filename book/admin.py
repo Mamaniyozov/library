b27e8e3d87c36d5abd6cb3dd4c8abf302286c8cd
Mamaniyozov <mamaniyozovmuhammadyusuf5@gmail.com>
2023-10-26 16:12:36 +0500

    all ready for work

--- book/admin.py
+++ book/admin.py
@@ -1,17 +1,24 @@
 from django.contrib import admin
 
-from .models import Genre, Publisher, Author, Book, User
+from .models import Genre, Publisher, Author, Book, User ,User_id , login_user ,Language  ,Reaction
 
 # admin.site.register(User)
 
 admin.site.register(Genre)
 
+admin.site.register(User)
 
 admin.site.register(Publisher)
 
 admin.site.register(Author)
 
+admin.site.register(User_id)
+
 admin.site.register(Book)
 
+admin.site.register(Language)
 
+admin.site.register(login_user)
+
+admin.site.register(Reaction)
 # Register your models here.
