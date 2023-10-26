8882f3c190fb4854d4f90cf82de67192f22803a5
KarimovProject <abdullatifkarimov772@gmail.com>
2023-10-26 15:36:41 +0500

    fixed

--- book/admin.py
+++ book/admin.py
@@ -1,15 +1,15 @@
 from django.contrib import admin
 
-from .models import Genre, Publisher, Autor, Book, User
+from .models import Genre, Publisher, Author, Book, User
 
-admin.site.register(User)
+# admin.site.register(User)
 
 admin.site.register(Genre)
 
 
 admin.site.register(Publisher)
 
-admin.site.register(Autor)
+admin.site.register(Author)
 
 admin.site.register(Book)
 
--- book/models.py
+++ book/models.py
@@ -59,7 +59,7 @@ class Author (models.Model):
 
 
 class Language(models.Model):
-    name = models.CharField(null=True , blank=True)
+    name = models.CharField(max_length=200, unique=True)
 
     def __str__(self):
         return self.name
