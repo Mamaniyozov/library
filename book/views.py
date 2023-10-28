96a109bf2c7d470e2d1063739bff1e92ca375dc9
Mamaniyozov <mamaniyozovmuhammadyusuf5@gmail.com>
2023-10-28 08:45:40 +0500

    create authenticated in library

--- book/views.py
+++ book/views.py
@@ -4,6 +4,8 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.request import Request
 from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
 class Users(APIView):
     def get(self, requsets):
         try:
